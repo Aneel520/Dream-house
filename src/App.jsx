f0f246529acc6bf508ba0936d7a12559ef63aa79
Aneel <anilarasada333@gmail.com>
2024-07-12 11:37:15 +0530

    install react js and tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const App = () => {
+  return (
+    <>
+      <div>
+        <h1 className="text-red-400">Hello world</h1>
+        <p>this is aneel </p>
+      </div>
+    </>
+  );
+};
+
+export default App;
